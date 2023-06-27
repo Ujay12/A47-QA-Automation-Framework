7e6e01ea6da8321a36020204a6a49c6ada4946b6
Ujay12 <132020084+Ujay12@users.noreply.github.com>
2023-06-27 17:49:44 -0500

    Create Homework16.java

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+@Test
+public class RegistrationTest {
+// navigate to the registration page
+driver.get("https://qa.koel.app/",)
+// perform your registration tests here
+// close the browser
+        driver.quit();
+}
